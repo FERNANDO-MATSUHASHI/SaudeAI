294ad98a3cbd856a21151eab0b4e4fdaad62710c
FERNANDO-MATSUHASHI <matsu.zf@gmail.com>
2024-10-04 08:54:00 -0300

    Alterações Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="q-pa-md row justify-center">
-    <q-card style="width: 100%; max-width: 850px; height: 550px;">
+  <div class="q-pa-md row justify-center chat-background">
+    <q-card class="card">
       <q-card-section class="text-h6">Chat</q-card-section>
 
-      <q-card-section class="scroll-area" style="height: 450px; overflow-y: auto;" ref="chatArea">
+      <q-card-section virtual-scroll :virtual-scroll-item-size="50" style="max-height: 570px; overflow-y: auto;">
         <q-chat-message
           v-for="(message, index) in messages"
           :key="index"
@@ -18,7 +18,7 @@
         </q-chat-message>
 
         <!-- Exibe GIF de loading se estiver aguardando resposta -->
-        <q-chat-message v-if="loading" name="Aguardando..." avatar="public/logo/Logo.png" :sent="false" text-color="black" bg-color="amber">
+        <q-chat-message v-if="loading" name="Aguardando..." avatar="https://github.com/FERNANDO-MATSUHASHI/SaudeAI/blob/main/public/logo/Logo.png?raw=true" :sent="false" text-color="black" bg-color="amber">
           <div class="loading-message">
             <svg class="q-spinner" fill="currentColor" width="2rem" height="2rem" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg">
               <circle cx="15" cy="15" r="15">
@@ -38,30 +38,35 @@
         </q-chat-message>
       </q-card-section>
     </q-card>
+    <!-- Card para Input e Botão -->
+    <q-card class="card_button">
+      <q-card-section>
+        <div class="row items-center q-gutter-sm">
+          <q-input
+            v-model="userMessage"
+            ref="userInput"
+            filled
+            placeholder="Digite sua mensagem..."
+            @keyup.enter="sendMessage"
+            class="col dark-input"
+          />
+          <q-btn
+            round
+            icon="send"
+            color="primary"
+            @click="sendMessage"
+            :disable="!userMessage"
+          />
+        </div>
+      </q-card-section>
+    </q-card>
 
-    <div style="width: 100%; max-width: 850px; margin-top: 10px;">
-      <div class="row items-center q-gutter-sm">
-        <q-input
-          v-model="userMessage"
-          filled
-          placeholder="Digite sua mensagem..."
-          @keyup.enter="sendMessage"
-          class="col"
-        />
-        <q-btn
-          round
-          icon="send"
-          color="primary"
-          @click="sendMessage"
-          :disable="!userMessage"
-        />
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import './MainLayout.css';
 
 export default {
   data() {
@@ -71,7 +76,7 @@ export default {
       messages: [
         {
           name: "Chat",
-          avatar: "public/logo/Logo.png",
+          avatar: "https://github.com/FERNANDO-MATSUHASHI/SaudeAI/blob/main/public/logo/Logo.png?raw=true",
           stamp: "Agora",
           content: "Olá! Como posso ajudar você?",
           sent: false,
@@ -81,97 +86,66 @@ export default {
       ],
     };
   },
+  mounted() {
+    this.$refs.userInput.focus(); // Foca no input ao montar o componente
+  },
   methods: {
-  async sendMessage() {
-    if (this.userMessage.trim()) {
-      const messageToSend = this.userMessage;
-      this.userMessage = ""; 
+    async sendMessage() {
+      if (this.userMessage.trim()) {
+        const messageToSend = this.userMessage;
 
-      this.messages.push({
-        name: "Você",
-        avatar: "https://cdn.quasar.dev/img/avatar3.jpg",
-        stamp: new Date().toLocaleTimeString(),
-        content: messageToSend,
-        sent: true,
-        textColor: "white",
-        bgColor: "primary",
-      });
-
-      this.scrollToBottom(); // Rolar para a última mensagem
-
-      this.loading = true; // Começa o loading
-
-      try {
-        const response = await axios.post(
-          "https://chatllama-tw11.onrender.com/api/chat",
-          {
-            mensagem_usuario: messageToSend,
-          }
-        );
+        this.userMessage = "";
 
         this.messages.push({
-          name: "Chat",
-          avatar: "public/logo/Logo.png",
+          name: "Você",
+          avatar: "https://github.com/FERNANDO-MATSUHASHI/SaudeAI/blob/main/public/logo/Person.png?raw=true",
           stamp: new Date().toLocaleTimeString(),
-          content: response.data.resposta || "Resposta não disponível.",
-          sent: false,
-          textColor: "black",
-          bgColor: "amber",
-        });
-
-        this.scrollToBottom(); // Rolar para a última mensagem
-      } catch (error) {
-        console.error("Erro ao enviar mensagem:", error);
-        this.messages.push({
-          name: "Erro",
-          avatar: "https://cdn.quasar.dev/img/avatar5.jpg",
-          stamp: new Date().toLocaleTimeString(),
-          content: "Não foi possível obter a resposta do servidor.",
-          sent: false,
+          content: messageToSend,
+          sent: true,
           textColor: "white",
-          bgColor: "negative",
+          bgColor: "primary",
         });
 
-        this.scrollToBottom(); // Rolar para a última mensagem
-      } finally {
-        this.loading = false; // Para o loading
-      }
-    }
-  },
-  scrollToBottom() {
-    this.$nextTick(() => {
-      const chatArea = this.$refs.chatArea;
-      if (chatArea) {
-        chatArea.scrollTop = chatArea.scrollHeight;
-      }
-    });
-  },
-},
+        this.loading = true; // Começa o loading
 
+        try {
+          const response = await axios.post(
+            "https://chatllama-tw11.onrender.com/api/chat",
+            {
+              mensagem_usuario: messageToSend,
+            }
+          );
+
+          this.messages.push({
+            name: "Chat",
+            avatar: "https://github.com/FERNANDO-MATSUHASHI/SaudeAI/blob/main/public/logo/Logo.png?raw=true",
+            stamp: new Date().toLocaleTimeString(),
+            content: response.data.resposta || "Resposta não disponível.",
+            sent: false,
+            textColor: "black",
+            bgColor: "amber",
+          });
+        } catch (error) {
+          console.error("Erro ao enviar mensagem:", error);
+          this.messages.push({
+            name: "Erro",
+            avatar: "https://github.com/FERNANDO-MATSUHASHI/SaudeAI/blob/main/public/logo/Logo.png?raw=true",
+            stamp: new Date().toLocaleTimeString(),
+            content: "Não foi possível obter a resposta do servidor.",
+            sent: false,
+            textColor: "white",
+            bgColor: "negative",
+          });
+        } finally {
+          this.loading = false; // Para o loading
+        }
+      }
+      this.$nextTick(() => {
+        if (this.$refs.scrollArea) {
+          this.$refs.scrollArea.scrollTop = this.$refs.scrollArea.scrollHeight;
+        }
+      });
+    },
+  },
 };
 </script>
-
-<style lang="sass">
-.my-emoticon
-  vertical-align: middle
-  height: 2em
-  width: 2em
-
-.scroll-area
-  height: 450px
-  overflow-y: auto
-
-.loading-message
-  display: flex
-  align-items: center
-  justify-content: flex-start // Alinha à esquerda
-  background-color: #ffc107 // Define o fundo amarelo (cor semelhante ao amber)
-  padding: 5px 10px
-  border-radius: 10px
-  margin-left: 10px
-
-.loading-gif
-  height: 20px
-  width: 20px
-  margin-right: 5px
-</style>
